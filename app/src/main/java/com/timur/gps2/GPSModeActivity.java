47a307a8e4216af466950fcd3d19a27e6de4bbf5
Timur <glubberer15@web.de>
2017-06-08 12:00:32 +0200

    GPSModeActivity updated

--- app/src/main/java/com/timur/gps2/GPSModeActivity.java
+++ app/src/main/java/com/timur/gps2/GPSModeActivity.java
@@ -1,10 +1,8 @@
 package com.timur.gps2;
 
-import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.ImageView;
 
 public class GPSModeActivity extends AppCompatActivity {
