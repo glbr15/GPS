a0d9a77f82c7ae06a89b58e4a77b66b5ae14b866
Timur <glubberer15@web.de>
2017-06-18 22:09:39 +0200

    Beginn: LocationsFragment

---
+++ app/src/main/java/com/timur/gps2/LoacationsFragment.java
@@ -0,0 +1,22 @@
+package com.timur.gps2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoacationsFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_locations, container, false);
+        return rootView;
+    }
+}
